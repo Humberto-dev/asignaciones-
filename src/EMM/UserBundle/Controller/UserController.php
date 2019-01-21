e708fa3e5dc28330d06116d465a791e796efbbe6
humberto <humberto@w3bgurus.com>
2019-01-21 18:10:42 -0430

    creando formaulario para crear usuario

--- src/EMM/UserBundle/Controller/UserController.php
+++ src/EMM/UserBundle/Controller/UserController.php
@@ -4,6 +4,8 @@ namespace EMM\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use EMM\UserBundle\Entity\User;
+use EMM\UserBundle\Form\UserType;
 
 class UserController extends Controller
 {
@@ -12,14 +14,16 @@ class UserController extends Controller
         $em= $this->getDoctrine()->getManager();
         $users= $em->getRepository('EMMUserBundle:User')->findAll();
 
-        $res='Lista de usarios: <br />';
+       /* $res='Lista de usarios: <br />';
         
         foreach($users as $user)
         {
             $res .= 'Usuario:'.$user->getUsername(). ' -Email:'. $user->getEmail(). '<br/>';
         }
 
-        return new Response($res);
+        return new Response($res);*/
+
+        return $this->render('EMMUserBundle:User:index.html.twig', array('users'=>$users));
     }
 
     public function viewAction($id)
@@ -28,6 +32,25 @@ class UserController extends Controller
         $user= $repository->find($id);
 
         return new Response('Usuario '. $user->getusername(). ' con Email '. $user->getEmail());
+    }
+
+    public function addAction()
+    {
+        $user= new User();
+        $form= $this->createCreateForm($user);
+
+        return $this->render('EMMUserBundle:User:add.html.twig', array('form'=>$form->createView()));
+        
+    }
+
+    public function createCreateForm(User $entity)
+    {
+        $form= $this->createForm(new userType(),$entity,array(
+            'action'=>$this->generateURL('emm_user_create'),
+            'method'=>'POST'
+        ));
+
+        return $form;
     }
 }
 
